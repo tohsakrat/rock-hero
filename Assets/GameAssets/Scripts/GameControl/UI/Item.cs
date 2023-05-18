3db7bf151d888aeb1d3bc7cda40d6739914413d5
Girafemmm <girafe6h@gmail.com>
2023-05-18 19:10:57 +0800

    UI更新

---
+++ Assets/GameAssets/Scripts/GameControl/UI/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public Item item;
+    public Image itemImage;
+    //public Sprite itemImageSprite;
+}
--- Assets/GameAssets/Scripts/GameControl/UI/Shop.cs
+++ Assets/GameAssets/Scripts/GameControl/UI/Shop.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
+    //static Shop instance;
+
+    public GameObject ItemGrid;
+    public Item item;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //int num = Hero.r.items.Count;
+        //Debug.Log("物品栏数量:");
+        //Debug.Log(num);
+
+
+        //foreach (Pickup i in Hero.r.items)
+        //{
+        //    Debug.Log("物品名称：");
+        //    Debug.Log(i.name);
+        //    CreateNewItem(item, ItemGrid, i.sp);
+        //}
+
+        //foreach (skills s in hero.r.skills)
+        //{
+        //    debug.log("技能名称");
+        //    debug.log(i.name);
+        //}
+
+        //Debug.Log(Hero.r.Skills);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEnable()
     {
+        int num = Hero.r.items.Count;
+        Debug.Log("物品栏数量:");
+        Debug.Log(num);
+
+        foreach (Pickup i in Hero.r.items)
+        {
+            Debug.Log("物品名称：");
+            Debug.Log(i.name);
+            CreateNewItem(item, ItemGrid, i.sp);
+        }
+    }
+
+    public void OnDisable()
+    {
+        ClearItem(ItemGrid);
+    }
+
+
+    public static void CreateNewItem(Item item, GameObject ItemGrid, Sprite itemSprite)
+    {
+        Item newItem = Instantiate(item, ItemGrid.transform.position,Quaternion.identity);
+        newItem.gameObject.transform.SetParent(ItemGrid.transform);
         
+        newItem.item = item;
+        newItem.itemImage.sprite = itemSprite;
+    }
+
+    private void ClearItem(GameObject ItemGrid)
+    {
+        if(ItemGrid.transform.childCount > 0)
+        {
+            for (int i =0; i < ItemGrid.transform.childCount; i++)
+            {
+                Destroy(ItemGrid.transform.GetChild(i).gameObject);
+            }
+        }
     }
 }
